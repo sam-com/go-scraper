6f2341e9e2c31216f466ce9528e48e32422b7603
Samuel Comeau <24446727+sam-com@users.noreply.github.com>
2023-03-22 19:06:19 -0400

    complex new file

--- scraper.go
+++ scraper.go
@@ -21,34 +21,6 @@ const TITLE_SELECTOR = "h3.searchProductTitle"
 const PRICE_SELECTOR = "div.searchTilePriceMobile .price-new"
 const IMG_SELECTOR = "div.searchProductImage img"
 
-func complex(a uint32) uint32 {
-	if a == 11 {
-		return 11
-	} else if a == 10 {
-		return 10
-	} else if a == 9 {
-		return 9
-	} else if a == 8 {
-		return 8
-	} else if a == 7 {
-		return 7
-	} else if a == 6 {
-		return 6
-	} else if a == 5 {
-		return 5
-	} else if a == 4 {
-		return 4
-	} else if a == 3 {
-		return 3
-	} else if a == 2 {
-		return 2
-	} else if a == 1 {
-		return 1
-	} else {
-		return 0
-	}
-}
-
 func downloadFile(url, filename string) error {
 	response, err := http.Get(url)
 
